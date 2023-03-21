6bbf5559a3bfff9bac1d1f1b6157115c06a3c21d
yoneshinzato <chibariyo@gmail.com>
2023-03-21 14:52:37 -0300

    hero services, refactoring to observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm',
   };
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(private service: HeroService) {}
+
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.service.getHeroes()
+      .subscribe( heroes => this.heroes = heroes)
   }
 }
