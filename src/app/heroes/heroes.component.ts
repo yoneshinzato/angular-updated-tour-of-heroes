72bec270e0bd0bc8264ce751674c449fc185ace8
yoneshinzato <chibariyo@gmail.com>
2023-03-21 14:52:46 -0300

    1 file modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,7 +28,6 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.service.getHeroes()
-      .subscribe( heroes => this.heroes = heroes)
+    this.service.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
